08021e70dcf72a8f39ac590571057e6393f08a59
fnsl223 <fnsl223@naver.com>
2024-09-02 13:35:31 +0900

    Remove duplicate project folder and reorganize files

--- vss_ver4.2/src/main/java/jvbz/boot/vss/inquiry/repository/InquiryRepository.java
+++
@@ -1,9 +0,0 @@
-package jvbz.boot.vss.inquiry.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jvbz.boot.vss.inquiry.entity.Inquiry;
-
-public interface InquiryRepository extends JpaRepository<Inquiry, Integer> {
-	void deleteByUserId(String userId);
-}
